a573c905d17fec0ac7bcece478731189ccd8e251
Jihoon Chung <j.c@navercorp.com>
2014-03-19 11:36:05 +0900

    added .makesymlinks.sh

---
+++ .makesymlinks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# vim: ts=8 sw=8 noet
+########## Variables
+
+olddir=dotfiles_old             # old dotfiles backup directory
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in *; do
+	echo $file
+	echo "Moving any existing dotfiles from ~ to $olddir"
+	mv -f ~/.$file dotfiles_old/
+	echo "Creating symlink to $file in home directory."
+	ln -s `pwd`/$file ~/.$file
+done
