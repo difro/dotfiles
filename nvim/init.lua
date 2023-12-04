37daa58a078ea4e52ec088e7bc455bdb936b8d63
Jihoon Chung <j.c@navercorp.com>
2023-12-04 12:59:10 +0900

    init.lua: add border to documentation

--- nvim/init.lua
+++ nvim/init.lua
@@ -589,7 +589,8 @@ cmp.setup {
     { name = 'luasnip' },
   },
   window = {
-    completion = cmp.config.window.bordered(),  
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
 }
 
