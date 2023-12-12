6d7ba5e679e2e3aa32fae56d8bc5a5db9aba26c1
Jihoon Chung <j.c@navercorp.com>
2023-12-13 08:37:28 +0900

    Add sunglasses.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -267,18 +267,26 @@ require('lazy').setup({
 
   {
     "ray-x/go.nvim",
-    dependencies = {  -- optional packages
-    "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   },
-  config = function()
-    require("go").setup()
-  end,
-  event = {"CmdlineEnter"},
-  ft = {"go", 'gomod'},
-  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-}
+
+  {
+    "miversen33/sunglasses.nvim",
+    opts = {
+      filter_type = "SHADE",
+      filter_percent = .35,
+    }
+  },
 
 
 
