c5ffb3b95480d0f5a481d9340768388591f526c6
Jihoon Chung <j.c@navercorp.com>
2024-11-08 18:21:01 +0900

    Update nvim-notify setup with background colour configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -583,6 +583,9 @@ require('lazy').setup({
     "rcarriga/nvim-notify",
     config = function()
       vim.notify = require("notify")
+      require("notify").setup({
+        background_colour = "#000000",
+      })
     end,
   },
 
