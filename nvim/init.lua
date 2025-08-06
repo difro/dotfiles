fc7edc9e375ba75c8d0620784bdfc3b321d8b0ff
Jihoon Chung <jihoon@gmail.com>
2025-08-06 22:34:34 +0900

    Update AeroSpace keybindings, UI tweaks, and add DracAura/Monokai theme support.

--- nvim/init.lua
+++ nvim/init.lua
@@ -297,6 +297,23 @@ require('lazy').setup({
     end,
   },
 
+  {
+    "Mofiqul/dracula.nvim",
+    -- config = function()
+    --   vim.cmd.colorscheme 'dracula'
+    -- end,
+  },
+
+  {
+    "loctvl842/monokai-pro.nvim",
+    config = function()
+      require("monokai-pro").setup({
+        transparent_background = true,
+        filter = "pro", -- classic | octagon | pro | ristretto | spectrum | machine
+      })
+      -- vim.cmd.colorscheme 'monokai-pro'
+    end
+  },
 
   {
     -- Set lualine as statusline
