ffafef7f8667afc17bcacc4086541489ca652991
Jihoon Chung <j.c@navercorp.com>
2026-05-18 11:31:44 +0900

    Pin nvim-treesitter to master branch
    The main branch (v1.0+) removed the nvim-treesitter.configs module,
    breaking the existing setup() call. Pin to master to keep the legacy
    config API working until a full migration.

--- nvim/init.lua
+++ nvim/init.lua
@@ -294,6 +294,7 @@ local plugins = {
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
     build = ':TSUpdate',
     config = function()
       require'nvim-treesitter.configs'.setup {
