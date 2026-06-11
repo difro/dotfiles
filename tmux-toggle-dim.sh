0dfb7078d244089a38d020bf5deeda88de1e8c47
Jihoon Chung <j.c@navercorp.com>
2026-06-11 13:32:16 +0900

    Make tmux focus dim opt-in

--- tmux-toggle-dim.sh
+++ tmux-toggle-dim.sh
@@ -6,16 +6,16 @@ set -u
 
 active="$1"
 
-if [ "$(tmux show -wv @dim-disabled 2>/dev/null || true)" = "1" ]; then
-    tmux set -wu @dim-disabled
+if [ "$(tmux show -wv @dim-enabled 2>/dev/null || true)" = "1" ]; then
+    tmux set -w @dim-enabled 0
+    while IFS= read -r pane; do
+        tmux select-pane -t "$pane" -P fg=default
+    done < <(tmux list-panes -F '#{pane_id}')
+else
+    tmux set -w @dim-enabled 1
     while IFS= read -r pane; do
         [ "$pane" = "$active" ] && continue
         tmux select-pane -t "$pane" -P fg=colour245
-    done < <(tmux list-panes -F '#{pane_id}')
-else
-    tmux set -w @dim-disabled 1
-    while IFS= read -r pane; do
-        tmux select-pane -t "$pane" -P fg=default
     done < <(tmux list-panes -F '#{pane_id}')
 fi
 
